995e2da25f82968e733e2b2a1edee18e0ce2192e
mariaaa <toledomiranda.maria23@gmail.com>
2025-06-03 16:16:19 -0400

    bloco de notas criado

--- Bloco de Notas/main.py
+++ Bloco de Notas/main.py
@@ -1,16 +1,48 @@
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
-from blocoNotas import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from blocoNotas import Ui_MainWindow 
 
-class Main(QMainWindow):
-    def __init__(sdelf):
+class EditorTexto(QMainWindow):
+    def __init__(self):
         super().__init__()
-        self.ui = ui.MainWindow()
+        self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+ 
+# Conectando as ações aos métodos
+        self.ui.actionNovo.triggered.connect(self.novo_arquivo)
+
+        self.ui.actionAbrir.triggered.connect(self.abrir_arquivo)
+
+        
+        self.ui.actionSalvar.triggered.connect(self.salvar_arquivo)
+
+
+        self.ui.actionSair.triggered.connect(self.close)
+
+
+    def novo_arquivo(self):
+        self.ui.textEdit.clear()
+    
+
+
+
+
+    def abrir_arquivo(self):
+        caminho = QFileDialog.getOpenFileName(self, "Abrir Arquivo", "", "Text Files (*.txt)")[0]
+        if caminho:
+            with open(caminho, 'r', encoding='utf-8') as f:
+                self.ui.textEdit.setText(f.read())
+        
+        
+    def salvar_arquivo(self):
+        caminho = QFileDialog.getSaveFileName(self, "Salvar Arquivo", "", "Text Files (*.txt)")[0]
+        if caminho:
+            with open(caminho, 'w', encoding='utf-8') as f:
+                f.write(self.ui.textEdit.toPlainText())
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = Main()
+    window = EditorTexto()
     window.show()
     sys.exit(app.exec_())
